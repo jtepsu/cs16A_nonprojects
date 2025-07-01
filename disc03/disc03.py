15d5987a9d6fb506e2c94a3fcc0159a448b600a2
jtepsu <jitrada.tep@gmail.com>
2025-07-01 21:53:54 +0700

    disc03: finished everything up to sevens game

---
+++ disc03/disc03.py
@@ -0,0 +1,66 @@
+# Q1
+def swipe(n):
+    if n >= 10:
+        print(n % 10)
+        swipe(n // 10)
+        print(n % 10)
+    else:
+        print(n)
+
+# Q2
+def skip_factorial(n):
+    if n <= 2:
+        return n
+    else:
+        return n * skip_factorial(n - 2)
+
+# Q3
+def is_prime(n):    
+    "checks whether n can be evenly divided by any number between 1 and n"
+    def f(i):        
+        if n % i == 0:            
+            return False        
+        elif n - i == 1:            
+            return True        
+        else:            
+            return f(i + 1)    
+    return f(2)
+
+# Q4
+def hailstone(n):
+    steps = 1
+    print(int(n))
+    if n % 2 == 0:
+        steps += hailstone(n / 2)
+    elif n > 1:
+        steps += hailstone((n * 3) + 1)
+    return steps
+
+# print(hailstone(10))
+
+# Q5
+def has_seven(n):
+    if n < 7:
+        return False
+    elif n % 7 == 0:
+        return True
+    else:
+        while n >= 7:
+            if n % 10 == 7:
+                return True
+            n = n // 10
+        return False
+
+def sevens(n, k):
+    i = 1
+    who = 1
+    direction = 1
+    print(f"Player {who} says {i}.")
+    if has_seven(i):
+        direction = direction * (-1)
+    i += 1
+    if i == n:
+        sevens(n, k)
+
+        
+
